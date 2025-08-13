d4dd532587a344510b23b124ca190c4c5e7e3308
Hafiz Rayan Kurniawan <fiz@hrk.my.id>
2025-08-13 20:31:42 +0700

    Enhances heading styling in Home component

--- src/app/(main)/test/page.tsx
+++ src/app/(main)/test/page.tsx
@@ -3,7 +3,7 @@ import { HydrateClient } from "~/trpc/server";
 export default async function Home() {
   return (
     <HydrateClient>
-        <h1>hello</h1>
+        <h1 className="flex flex-1 flex-col gap-6 p-6">hello</h1>
     </HydrateClient>
   );
 }
