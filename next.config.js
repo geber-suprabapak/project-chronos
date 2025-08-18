8446f6a336687c2faa16112e5492a4b6abb429df
Hafiz Rayan Kurniawan <fiz@hrk.my.id>
2025-08-18 08:50:27 +0700

    Update .gitignore and package files to include new dependencies and configurations

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uacjwtyhlrwojwqdanop.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+};
 
 export default config;
