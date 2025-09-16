568418a6da0478af9a03aeacd2927c53a1b94afb
Naufal Pinandhita <gamer117095n4uf4l@gmail.com>
2025-09-16 12:20:02 +0700

    refactor: remove unnecessary TypeScript ESLint disables in ProfilesPage

--- src/app/(main)/profiles/page.tsx
+++ src/app/(main)/profiles/page.tsx
@@ -1,10 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
-
 import Link from "next/link";
 import { api } from "~/trpc/server";
 import {
